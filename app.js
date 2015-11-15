38dd6da631f0409f15167b90e0fda073b28ca614
Sheng Yunzhou <shengyunzhou@gmail.com>
2015-11-15 15:04:01 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by shengyunzhou on 15/11/15.
+ */
+
+'use strict';
+
+var data = [[4, 8, 7, 3], [2, 5, 9, 3], [6, 3, 2, 5], [4, 4, 1, 6]];
+
+var BOUND = 4;
+
+var steps = [];
+var alreadyTakenCoord = [];
+
+function findNextStep (x, y) {
+    // boundary check
+
+}
