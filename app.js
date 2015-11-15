9d9e4a224e47757c292eef3913a10d01f93c1da8
Sheng Yunzhou <shengyunzhou@gmail.com>
2015-11-15 18:21:00 +0800

    [YZ] - Complete the first draft of the solution

--- app.js
+++ app.js
@@ -3,60 +3,25 @@
  */
 
 'use strict';
-
-var _ = require('lodash');
-
-var data = [[4, 8, 7, 3], [2, 5, 9, 3], [6, 3, 2, 5], [4, 4, 1, 6]];
-
-var BOUND = 3;
-
-var steps = [];
-var alreadyTakenCoord = [];
-var potentialStartingPoints = [];
-var peaks = [], bottoms = [];
-
-function findStartingPoints(cutoff) {
-    for (var x = 0; x < BOUND; x++) {
-        for (var y = 0; y < BOUND; y++) {
-            if (data[x][y] > cutoff) {
-                potentialStartingPoints.push([x, y]);
-            }
-        }
-    }
-}
-
+var fs = require('fs');
+var rawData;
+var BOUND;
+var peaks = [], allRoutes = [];
 
 function isHigherThanEast(x, y) {
-    return data[x][y] > data[x][y + 1];
+    return rawData[x][y] > rawData[x][y + 1];
 }
 
 function isHigherThanSouth(x, y) {
-    return data[x][y] > data[x + 1][y];
+    return rawData[x][y] > rawData[x + 1][y];
 }
 
 function isHigherThanWest(x, y) {
-    return data[x][y] > data[x][y - 1];
+    return rawData[x][y] > rawData[x][y - 1];
 }
 
 function isHigherThanNorth(x, y) {
-    return data[x][y] > data[x - 1][y];
-}
-
-
-function isLowerThanEast(x, y) {
-    return data[x][y] < data[x][y + 1];
-}
-
-function isLowerThanSouth(x, y) {
-    return data[x][y] < data[x + 1][y];
-}
-
-function isLowerThanWest(x, y) {
-    return data[x][y] < data[x][y - 1];
-}
-
-function isLowerThanNorth(x, y) {
-    return data[x][y] < data[x - 1][y];
+    return rawData[x][y] > rawData[x - 1][y];
 }
 
 function isPeak(x, y) {
@@ -79,42 +44,146 @@ function isPeak(x, y) {
     }
 }
 
-function isBottom(x, y) {
-    if (x === 0 && y === 0) {
-        return isLowerThanEast(x, y) && isLowerThanSouth(x, y);
-    } else if (x === 0 && y === BOUND) {
-        return isLowerThanWest(x, y) && isLowerThanSouth(x, y);
-    } else if (x === BOUND && y === 0){
-        return isLowerThanEast(x, y) && isLowerThanEast(x, y);
-    } else if (x === 0) {
-        return isLowerThanEast(x, y) && isLowerThanSouth(x, y) && isLowerThanWest(x, y);
-    } else if (y === 0) {
-        return isLowerThanNorth(x, y) && isLowerThanEast(x, y) && isLowerThanSouth(x, y);
-    } else if (x === BOUND) {
-        return isLowerThanEast(x, y) && isLowerThanNorth(x, y) && isLowerThanWest(x, y);
-    } else if (y === BOUND) {
-        return isLowerThanNorth(x, y) && isLowerThanWest(x, y) && isLowerThanSouth(x, y);
-    } else {
-        return isLowerThanEast(x, y) && isLowerThanNorth(x, y) && isLowerThanWest(x, y) && isLowerThanSouth(x, y);
+function tryEast(x, y, steps) {
+    if (isHigherThanEast(x, y)) {
+        steps.push([x, y + 1]);
+    }
+}
+function trySouth(x, y, steps) {
+    if (isHigherThanSouth(x, y)) {
+        steps.push([x + 1, y]);
     }
 }
 
-function findPeaksAndBottoms() {
+function tryWest(x, y, steps) {
+    if (isHigherThanWest(x, y)) {
+        steps.push([x, y - 1]);
+    }
+}
+function tryNorth(x, y, steps) {
+    if (isHigherThanNorth(x, y)) {
+        steps.push([x - 1, y]);
+    }
+}
+
+function getPossibleSteps (point) {
+    var steps = [], x = point[0], y = point[1];
+    if (x === 0 && y === 0) {
+        tryEast(x, y, steps);
+        trySouth(x, y, steps);
+    } else if (x === 0 && y === BOUND) {
+        tryWest(x, y, steps);
+        trySouth(x, y, steps);
+    } else if (x === BOUND && y === 0){
+        tryNorth(x, y, steps);
+        tryEast(x, y, steps);
+    } else if (x === 0) {
+        tryWest(x, y, steps);
+        tryEast(x, y, steps);
+        trySouth(x, y, steps);
+    } else if (y === 0) {
+        tryNorth(x, y, steps);
+        tryEast(x, y, steps);
+        trySouth(x, y, steps);
+    } else if (x === BOUND) {
+        tryNorth(x, y, steps);
+        tryEast(x, y, steps);
+        tryWest(x, y, steps);
+    } else if (y === BOUND) {
+        tryNorth(x, y, steps);
+        tryWest(x, y, steps);
+        trySouth(x, y, steps);
+    } else {
+        tryNorth(x, y, steps);
+        tryWest(x, y, steps);
+        trySouth(x, y, steps);
+        tryEast(x, y, steps);
+    }
+    return steps
+}
+
+
+function findPeaks() {
     for (var x = 0; x <= BOUND; x++) {
         for (var y = 0; y <= BOUND; y++) {
-            console.log("testing for " + x + ", " + y);
             if (isPeak(x, y)) {
                 peaks.push([x, y]);
             }
-
-            if (isBottom(x, y)) {
-                bottoms.push([x, y]);
-            }
         }
     }
-
-    console.log(peaks);
-    console.log(bottoms);
 }
 
-findPeaksAndBottoms();
+function findNext(point, route) {
+    route.push(point);
+    var possibleNextPoints = getPossibleSteps(point);
+
+    possibleNextPoints.forEach(function (nextPoint) {
+
+        var currentRoute = route.slice(0); // take a copy of the route so far
+
+        if (getPossibleSteps(nextPoint).length === 0) {
+            currentRoute.push(nextPoint);
+            allRoutes.push(currentRoute);
+        } else {
+            findNext(nextPoint, currentRoute);
+        }
+    });
+}
+
+function generateRoutes() {
+    peaks.forEach(function (peak) {
+        var currentRoute = [];
+        findNext(peak, currentRoute);
+    });
+}
+
+function getLongestChild(routes) {
+    var longest = 0;
+    routes.forEach(function (child) {
+        if (child.length > longest) {
+            longest = child.length;
+        }
+    });
+
+    return longest;
+}
+
+function getBiggestDrop(routes, length) {
+    var drop = 0;
+    routes.forEach(function (child) {
+        if (child.length === length) {
+            var startingPoint = child[0],
+                endingPoint = child[length - 1],
+                diff = rawData[startingPoint[0]][startingPoint[1]] - rawData[endingPoint[0]][endingPoint[1]];
+            if (drop < diff) {
+                drop = diff;
+            }
+        }
+    });
+
+    return drop;
+}
+
+fs.readFile('data.txt', 'utf-8', function (err, data) {
+    if (err) throw err;
+    console.log('reading data');
+    var mockData = data.split(/\n/);
+    mockData.shift();
+    rawData = mockData.map(function (row) {
+        return row.split(/\s/).map(function (number) {
+            return parseInt(number);
+        });
+    });
+
+    BOUND = rawData[0].length - 1;
+
+    findPeaks();
+    generateRoutes();
+
+    console.log("The longest distance is " + getLongestChild(allRoutes));
+    console.log("The drop for " + getLongestChild(allRoutes) + " steps is " + getBiggestDrop(allRoutes, getLongestChild(allRoutes)));
+
+});
+
+
+
